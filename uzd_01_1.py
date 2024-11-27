aa243d07049f38f83bbf67af49131d80f8ea1b3f
MadarsPodnieks <madars.podnieks@grobinasskola.lv>
2024-11-27 14:30:43 +0200

    uzrakstiju šo

--- uzd_01_1.py
+++ uzd_01_1.py
@@ -1,3 +1,6 @@
 # Uzdevums: 01-1: Izveidot programmu, kas prasa lietotâjam ievadît skaitli n, tad programma aprçíina n+nn+nnn. 
-Rezultâts tiek parâdîts konsolç.
+#Rezultâts tiek parâdîts konsolç.
+n=int(input("ievadi skaitli:"))
+aprekins=n+nn+nnn
+print(f"{n}+{n}{n}+{n}{n}{n} ir {aprekins}")
 
--- uzd_01_2.py
+++ uzd_01_2.py
@@ -1,5 +1,14 @@
 # Izveidot programmu, kura prasa lietotâjam ievadît cilindra râdiusu un tâ augstumu, tiek aprçíinâts cilindra laukums un tilpums. Rezultâts tiek parâdîts konsolç.
 # tilpums = 3.14 * râdiuss * râdiuss * augstums
 # laukums = 2 * (3.14 * râdiuss * râdiuss) + augstums * (2 * 3.14 * râdiuss)
+import math
+pi=math.pi
+radius=int(input("ievadi cilindra rādiusu:"))
+h=int(input("ievadi cilindra augstumu:"))
+tilpums= pi*radius *h
+laukums=2* pi*radius**2 +h *2*pi *radius
+print(f"Cilindra laukums ir {laukums:.3f}, bet tilpums ir {tilpums:.3f}")
+
+
 
 
